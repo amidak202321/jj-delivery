ac5567b7fa2752c86f8b3d8c7280f7a13c29abbe
amidak202321 <matheuskadimo2@gmail.com>
2026-08-22 17:26:29 -0300

    Atualiza versão do APK para 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.jjburger.entregador"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 }
 dependencies {
